3baa24065dae2fd65d9705111d069f3d3cad2b7e
santraaj <santra.jantunen@gmail.com>
2022-03-22 23:43:33 +0200

    Osa 1 valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
-function App() {
+function Todolist() {
+  const [todo, setTodo] = useState({description: '', date: '', priority:''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = (event) => {
+    setTodos([...todos, todo]);
+  }
+
+  const columns = [
+    { headerName: 'Date', field: 'date' },
+    { headerName: 'Description', field: 'description' },
+    { headerName: 'Priority', field: 'priority' }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={inputChanged} placeholder="Description" name="description" value={todo.description}/>
+      <input type="text" onChange={inputChanged} placeholder="Date" name="date" value={todo.date}/>
+      <input type="text" onChange={inputChanged} placeholder="Priority" name="priority" value={todo.priority}/>
+      <button onClick={addTodo}>Add</button>
+      <div
+        className="ag-theme-material"
+        style={{
+        height: '700px',
+        width: '80%',
+        margin: 'auto'}}
+      >
+        <AgGridReact
+          columnDefs={columns}
+          rowData={todos}
+        >  
+        </AgGridReact>
+      </div> 
     </div>
   );
-}
+};
 
-export default App;
+export default Todolist;
